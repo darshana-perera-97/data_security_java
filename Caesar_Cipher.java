5042d84f80557895ddff124d1e0748bb8cf8daac
Darshana Perera <darshana.saluka.10@gmail.com>
2022-06-03 11:00:16 +0530

    java scanner library added

---
+++ Caesar_Cipher.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Caesar_Cipher{
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("x");
+        String text = obj.nextLine();
+        System.out.println(text);
+    }
+}
