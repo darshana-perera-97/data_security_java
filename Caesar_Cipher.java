9e53031c6166c49e3838674d8002c0704156f7d4
Darshana Perera <darshana.saluka.10@gmail.com>
2022-06-03 11:11:31 +0530

    Java plaintext read

--- Caesar_Cipher.java
+++ Caesar_Cipher.java
@@ -1,9 +1,18 @@
 import java.util.*;
-class Caesar_Cipher{
+
+class Caesar_Cipher {
     public static void main(String[] args) {
+        Caesar_Cipher c = new Caesar_Cipher();
+        c.getPlainText();
+
+        
+    }
+
+    void getPlainText() {
         Scanner obj = new Scanner(System.in);
-        System.out.println("x");
-        String text = obj.nextLine();
-        System.out.println(text);
+        System.out.print("Input the cipher text : ");
+        String ptxt = obj.nextLine();
+        System.out.println();
+        System.out.print("Your cipher text is " + ptxt);
     }
 }
