df216e2afca50153c98caf3c072bce0fc1ee6ab4
Darshana Perera <darshana.saluka.10@gmail.com>
2022-06-03 11:33:45 +0530

    got text size

--- Caesar_Cipher.java
+++ Caesar_Cipher.java
@@ -1,18 +1,25 @@
 import java.util.*;
 
 class Caesar_Cipher {
+
+    
+
     public static void main(String[] args) {
-        Caesar_Cipher c = new Caesar_Cipher();
-        c.getPlainText();
 
         
-    }
 
-    void getPlainText() {
+        // get the plain text
         Scanner obj = new Scanner(System.in);
         System.out.print("Input the cipher text : ");
         String ptxt = obj.nextLine();
         System.out.println();
+
+        // display plaintext
         System.out.print("Your cipher text is " + ptxt);
+
+        //get text size
+        int txtSize = ptxt.length();
+        System.out.println(txtSize);
     }
+
 }
